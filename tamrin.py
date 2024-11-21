3348f50d3517c5fbeaea6eae649da5cc27069d61
Shayan <mshn1398@gmail.com>
2024-11-21 19:29:03 +0330

    square root

---
+++ tamrin.py
@@ -0,0 +1,13 @@
+import math
+num = int(input("please enter a number: "))
+th = 0.00000000001
+if(num < 0):
+    print("invalid number")
+elif(num == 0):
+    print("0")
+elif(num > 1):
+    n1 = num
+    while(abs(n1 * n1 - num) > th):
+        n2 = (n1 + (num/n1))/2
+        n1 = n2
+    print(n1)
